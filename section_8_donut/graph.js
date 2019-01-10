42cb0caa370ac48543fd3303968089b06ff84638
nz25 <denis.zorenko@gmail.com>
2019-01-10 09:52:59 +0100

    ch8 legend

--- section_8_donut/graph.js
+++ section_8_donut/graph.js
@@ -19,10 +19,25 @@ const arcPath = d3.arc()
 
 const colour = d3.scaleOrdinal(d3['schemeSet3']);
 
+// legend setup
+const legendGroup = svg.append('g')
+    .attr('transform', `translate(${dims.width + 40}, 10)`);
+
+const legend = d3.legendColor()
+    .shape('circle')
+    .shapePadding(10)
+    .scale(colour);
+
+// update function
 const update = (data) => {
     
     // update colour scale domain
     colour.domain(data.map(d => d.name));
+
+    legendGroup.call(legend);
+
+    legendGroup.selectAll('text')
+        .attr('fill', 'white');
 
     // join enhanced (pie) data to path elements
     const paths = graph.selectAll('path')
