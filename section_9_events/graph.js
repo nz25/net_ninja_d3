81c025636bbc16f3e39d955e7a260d051ef219ec
nz25 <denis.zorenko@gmail.com>
2019-01-11 11:04:48 +0100

    ch9_events

--- section_9_events/graph.js
+++ section_9_events/graph.js
@@ -28,18 +28,6 @@ const legend = d3.legendColor()
     .shapePadding(10)
     .scale(colour);
 
-// tool tip
-const tip = d3.tip()
-    .attr('class', 'tip card')
-    .html(d => {
-        let content = `<div class="name">${d.data.name}</div>`;
-        content += `<div class="cost">${d.data.cost}</div>`;
-        content += `<div class="delete">Click slice to delete</div>`;
-        return content;
-    });
-
-graph.call(tip);
-
 // update function
 const update = (data) => {
     
@@ -76,18 +64,6 @@ const update = (data) => {
             .each(function(d) { this._current = d })
             .transition().duration(750)
                 .attrTween('d', arcTweenEnter);
-
-    // add events
-    graph.selectAll('path')
-        .on('mouseover', (d,i,n) => {
-            tip.show(d, n[i]);
-            handleMouseOver(d,i,n);
-        })
-        .on('mouseout', (d,i,n) => {
-            tip.hide();
-            handleMouseOut(d,i,n);
-        })
-        .on('click', handleClick);
 };
 
 
@@ -149,28 +125,4 @@ function arcTweenUpdate(d){
         return arcPath(i(t));
     }
 
-};
-
-
-// event handlers
-
-const handleMouseOver = (d, i, n) => {
-    //console.log(n[i]);
-    d3.select(n[i])
-        .transition('changeSliceFill').duration(300)
-            .attr('fill', '#fff');
-
-};
-
-const handleMouseOut = (d, i, n) => {
-    d3.select(n[i])
-        .transition('changeSliceFill').duration(300)
-            .attr('fill', colour(d.data.name));
-};
-
-const handleClick = (d) => {
-    //console.log(d);
-
-    const id = d.data.id;
-    db.collection('expenses').doc(id).delete();
 };
